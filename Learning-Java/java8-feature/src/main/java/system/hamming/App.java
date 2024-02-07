a61891f16e7bd957669dd6e38ad31eb8cf749a19
rajpalbs <rajpalbs@gmail.com>
2024-02-07 08:07:01 +0530

    Remove un-use module.

--- Learning-Java/java8-feature/src/main/java/system/hamming/App.java
+++
@@ -1,9 +0,0 @@
-package system.hamming;
-
-public class App {
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        System.out.println("Hello World!");
-        Class cls = App.class;
-        Object o = (App) cls.newInstance();
-    }
-}
